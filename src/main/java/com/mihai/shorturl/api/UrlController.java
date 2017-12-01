20ecbb8f07c13d21b360a52ec9a01e77eef52b42
Bram Ceulemans <bramceulemans@me.com>
2017-12-01 18:45:52 +0200

    Added descriptions to the URL Entity API model

--- src/main/java/com/mihai/shorturl/api/UrlController.java
+++ src/main/java/com/mihai/shorturl/api/UrlController.java
@@ -8,15 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/url")
 public class UrlController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(UrlController.class);
+    private static final Logger logger = LoggerFactory.getLogger(UrlController.class);
 
     private final UrlService service;
 
@@ -25,21 +22,17 @@ public class UrlController {
         this.service = srvc;
     }
 
-
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity shortenUrl(@RequestParam(value = "longUrl", defaultValue = "World") String url) {
-        LOGGER.debug("Shorten request for: {}", url);
-        UrlEntity urlEntity;
-        urlEntity = service.create(url);
-        LOGGER.debug("Returning url: {}", urlEntity.getHash());
-        return new ResponseEntity(urlEntity, HttpStatus.OK);
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UrlEntity> shortenUrl(@RequestParam(value = "longUrl", defaultValue = "World") String url) {
+        logger.debug("Shorten request for: {}", url);
+        UrlEntity urlEntity = service.create(url);
+        logger.debug("Returning url: {}", urlEntity.getHash());
+        return new ResponseEntity<>(urlEntity, HttpStatus.OK);
     }
 
-
-    @RequestMapping(method = RequestMethod.GET, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity enlargeUrl(@RequestParam(value = "shortUrl", defaultValue = "World") String shortUrl) {
-        UrlEntity urlEntity;
-        urlEntity = service.find(shortUrl);
-        return new ResponseEntity(urlEntity, HttpStatus.OK);
+    @GetMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UrlEntity> enlargeUrl(@RequestParam(value = "shortUrl", defaultValue = "World") String shortUrl) {
+        UrlEntity urlEntity = service.find(shortUrl);
+        return new ResponseEntity<>(urlEntity, HttpStatus.OK);
     }
 }
